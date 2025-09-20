77aa9c1c1371958a5e0e1eddeef69b158e787aa7
iyan subdiana <iyan_s@MacBook-Pro.local>
2025-09-20 12:49:48 +0700

    add env & register on app.module  for environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './infrastructure/prisma.module';
+import { RedisModule } from './infrastructure/redis.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule, 
+    RedisModule
+  ],
   controllers: [],
   providers: [],
 })
