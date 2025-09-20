4bb0256982f7cd4e14f32decede86139a5370276
iyan subdiana <iyan_s@MacBook-Pro.local>
2025-09-21 00:04:41 +0700

    add vehicle controller with REST CRUD endpoints

---
+++ src/interface/vehicle.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { VehicleService } from '../application/vehicle/vehicle.service';
+import { CreateVehicleDto, UpdateVehicleDto } from '../application/vehicle/vehicle.dto';
+import { JwtAuthGuard } from '../application/auth/jwt-auth.guard';
+
+@UseGuards(JwtAuthGuard) // Protect all vehicle routes
+@Controller('vehicles')
+export class VehicleController {
+    constructor(private vehicleService: VehicleService) { }
+
+    @Post()
+    create(@Body() createVehicleDto: CreateVehicleDto) {
+        return this.vehicleService.create(createVehicleDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.vehicleService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.vehicleService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateVehicleDto: UpdateVehicleDto) {
+        return this.vehicleService.update(id, updateVehicleDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.vehicleService.remove(id);
+    }
+}
