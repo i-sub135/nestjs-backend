bc5264481cbaf5ef5c5f5f78687d5f05ffb83694
iyan subdiana <iyan_s@MacBook-Pro.local>
2025-09-21 00:02:47 +0700

    add customer controller with REST CRUD endpoints

---
+++ src/interface/customer.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { CustomerService } from '../application/customer/customer.service';
+import { CreateCustomerDto, UpdateCustomerDto } from '../application/customer/customer.dto';
+import { JwtAuthGuard } from '../application/auth/jwt-auth.guard';
+
+@UseGuards(JwtAuthGuard) // Protect all customer routes
+@Controller('customers')
+export class CustomerController {
+    constructor(private customerService: CustomerService) { }
+
+    @Post()
+    create(@Body() createCustomerDto: CreateCustomerDto) {
+        return this.customerService.create(createCustomerDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.customerService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.customerService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
+        return this.customerService.update(id, updateCustomerDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.customerService.remove(id);
+    }
+}
