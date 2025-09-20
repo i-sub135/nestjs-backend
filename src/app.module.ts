747f4f5f104b1b657549136b0bd87ec9b1b2bab2
iyan subdiana <iyan_s@MacBook-Pro.local>
2025-09-21 00:08:39 +0700

    wire [auth, customer, vehicle] controllers and services to main app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './infrastructure/prisma.module';
 import { RedisModule } from './infrastructure/redis.module';
+import { AuthController } from './interface/auth.controller';
+import { CustomerController } from './interface/customer.controller';
+import { VehicleController } from './interface/vehicle.controller';
+import { AuthService } from './application/auth/auth.service';
+import { CustomerService } from './application/customer/customer.service';
+import { VehicleService } from './application/vehicle/vehicle.service';
+import { JwtAuthGuard } from './application/auth/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -12,7 +19,7 @@ import { RedisModule } from './infrastructure/redis.module';
     PrismaModule, 
     RedisModule
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController, CustomerController, VehicleController],
+  providers: [AuthService, CustomerService, VehicleService, JwtAuthGuard],
 })
 export class AppModule { }
