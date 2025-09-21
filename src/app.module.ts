47b89f5725ae04cacaa3a1e93f892871b388628c
iyan subdiana <iyan_s@MacBook-Pro.local>
2025-09-21 12:41:32 +0700

    wire up all controllers and services to main app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { RedisModule } from './infrastructure/redis.module';
 import { AuthController } from './interface/auth.controller';
 import { CustomerController } from './interface/customer.controller';
 import { VehicleController } from './interface/vehicle.controller';
+import { TrackingController } from './interface/tracking.controller';
+import { TrackingGateway } from './interface/tracking.gateway';
 import { AuthService } from './application/auth/auth.service';
 import { CustomerService } from './application/customer/customer.service';
 import { VehicleService } from './application/vehicle/vehicle.service';
+import { TrackingService } from './application/tracking/tracking.service';
 import { JwtAuthGuard } from './application/auth/jwt-auth.guard';
 
 @Module({
@@ -19,7 +22,7 @@ import { JwtAuthGuard } from './application/auth/jwt-auth.guard';
     PrismaModule, 
     RedisModule
   ],
-  controllers: [AuthController, CustomerController, VehicleController],
-  providers: [AuthService, CustomerService, VehicleService, JwtAuthGuard],
+  controllers: [AuthController, CustomerController, VehicleController, TrackingController],
+  providers: [AuthService, CustomerService, VehicleService, TrackingService, TrackingGateway, JwtAuthGuard],
 })
 export class AppModule { }
